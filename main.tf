bde019980fb82c4adea541f428a4484019d5278a
ahvnavs <ahvnav01@gmail.com>
2026-05-30 22:30:27 +0530

    add vpc and subnet

--- main.tf
+++ main.tf
@@ -1,4 +1,56 @@
 provider "aws" {
     region = var.default_region
 }
+provider "aws" {
+    alias = "east"
+    region = var.east_region
+}
 
+provider "aws" {
+    alias = "west"
+    region = var.west_region
+}
+
+resource "aws_vpc" "vpc_main" {
+    cidr_block = var.cidr_block[0]
+    provider = aws.east
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    enable_network_address_usage_metrics = true
+}
+resource "aws_subnet" "main_pub_sub" {
+    cidr_block = var.cidr_block[2]
+    vpc_id = aws_vpc.vpc_main.id
+    availability_zone = var.east_az[0]
+    map_public_ip_on_launch = true
+}
+resource "aws_subnet" "main_pri_sub" {
+    cidr_block = var.cidr_block[3]
+    vpc_id = aws_vpc.vpc_main.id
+    availability_zone = var.east_az[1]
+}
+resource "aws_internet_gateway" "main_igw" {
+    vpc_id = aws_vpc.vpc_main.id
+}
+
+resource "aws_vpc" "vpc_bkp" {
+    cidr_block = var.cidr_block[1]
+    provider = aws.west
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    enable_network_address_usage_metrics = true
+}
+resource "aws_subnet" "bkp_pub_sub" {
+    cidr_block = var.cidr_block[4]
+    vpc_id = aws_vpc.vpc_bkp.id
+    availability_zone = var.west_az[0]
+    map_public_ip_on_launch = true
+}
+resource "aws_subnet" "bkp_pri_sub" {
+    cidr_block = var.cidr_block[5]
+    vpc_id = aws_vpc.vpc_bkp.id
+    availability_zone = var.west_az[1]
+}
+resource "aws_internet_gateway" "bkp_igw" {
+    vpc_id = aws_vpc.vpc_bkp.id
+}
--- variable.tf
+++ variable.tf
@@ -3,3 +3,36 @@ variable "default_region" {
     type = string
     default = "ap-south-1"
 }
+variable "dr_az" {
+    description = "az for default region"
+    type = list(string)
+    default = [ "ap-south-1a","ap-south-1b","ap-south-1c" ]
+}
+
+variable "east_region" {
+    description = "east reagion"
+    type = string
+    default = "us-east-1"
+}
+variable "east_az" {
+    description = "az for east region"
+    type = list(string)
+    default = [ "us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f" ]
+}
+
+variable "west_region" {
+    description = "west region"
+    type = string
+    default = "us-west-1"
+}
+variable "west_az" {
+    description = "az for west region"
+    type = list(string)
+    default = [ "us-west-1a","us-west-1b","us-west-1c" ]
+}
+
+variable "cidr_block" {
+    description = "cidr_block for VPC and Subnet"
+    type = list(string)
+    default = ["10.0.0.0/16", "10.1.0.0/16","10.0.1.0/24","10.0.2.0/24","10.1.1.0/24","10.1.2.0/24"]
+}
