65fcb77a31a844bc04554766ffd92dfb10fca0e9
Abhinav Sharma <ahvnav01@gmail.com>
2026-05-30 20:03:14 +0000

    update backend

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
         bucket = "statefile-host-bucket"
         key = "houseofcards/terraform.tfstate"
         region = "ap-south-1"
-        dynamodb_table = "statefile-host-dynamo"
         encrypt = true
         use_lockfile = true
     }
